485268dbec4dc7aa4d37a53f970c6e61e3944024
gumpw <out_lier@126.com>
2014-07-10 20:43:17 +0800

    first commit

---
+++ com/simple8583/key/SimpleConstants.java
@@ -0,0 +1,14 @@
+package com.simple8583.key;
+
+public interface SimpleConstants {
+
+	
+//	public static String MSG_LENGTH = "MsgLength";
+	public static String MTI = "mti";
+	public static String TPDU = "tpdu";
+	public static String VERSION_NO = "VersionNo";
+	public static String BIT_MAP = "BitMap";
+	
+	public static String ENCODING = "GBK";
+	
+}
